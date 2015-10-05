6d018d1ca0c0f6aa023af1c6c481e653d0268f7f
Reshef Elisha <reshef.elisha@gmail.com>
2015-10-05 18:40:49 -0400

    Added proper error handling, REST, JSON
    RESTful everything!

--- BROApi.py
+++ BROApi.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 import random
 
 app = Flask(__name__)
 
 words = ['amigo','bra','brah','bro','broski','brotha','brother','bud','buddy','compadre','dawg','dude','guy','homeboy','homie','mate','pal']
 
-@app.route('/bro')
+@app.route('/bro', methods=['GET'])
 def broMe():
-    return random.choice(words)
+    return jsonify({'bro': random.choice(words)})
+
+@app.errorhandler(404)
+def 404_error(error):
+    return make_response(jsonify({'error': 'Not Found'}), 404)
+
 if __name__ == '__main':
     app.run()
